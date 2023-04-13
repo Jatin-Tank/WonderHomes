76f86edeed565c066cedac5038cd8fe6487e041f
Jatin-Tank <jatin.tank.official@gmail.com>
2023-04-14 00:19:34 +0530

    Signup & Login From Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
+import Login from "./components/Login/Login"
+import SignUp from "./components/SignUp/SignUp"
+import Dashboard from "./components/Dashboard/Dashboard"
 function App() {
  
 
   return (
-    <div className="App">
-      <h1>Happy Coding</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      {/* {user && <Route path="/" exact element={<Dashboard />} />} */}
+      <Route path="/" exact element={<Dashboard />}/>
+			<Route path="/signup" exact element={<SignUp />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,19 +1,11 @@
-import styles from './styles.module.css'
-import React from 'react'
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
 
-export default function SignUp() {
-
-    const [data, setData] = useState({
-		firstName: "",
-		lastName: "",
-		email: "",
-		password: "",
-	});
+const Login = () => {
+	const [data, setData] = useState({ email: "", password: "" });
 	const [error, setError] = useState("");
-	const navigate = useNavigate();
 
 	const handleChange = ({ currentTarget: input }) => {
 		setData({ ...data, [input.name]: input.value });
@@ -22,10 +14,10 @@ export default function SignUp() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		try {
-			const url = "http://localhost:8080/api/users";
+			const url = "http://localhost:8080/api/auth";
 			const { data: res } = await axios.post(url, data);
-			navigate("/login");
-			console.log(res.message);
+			localStorage.setItem("token", res.data);
+			window.location = "/";
 		} catch (error) {
 			if (
 				error.response &&
@@ -37,41 +29,12 @@ export default function SignUp() {
 		}
 	};
 
-
-
-  return (
-    
-     <div className={styles.signup_container}>
-			<div className={styles.signup_form_container}>
+	return (
+		<div className={styles.login_container}>
+			<div className={styles.login_form_container}>
 				<div className={styles.left}>
-					<h1>Welcome Back</h1>
-					<Link to="/login">
-						<button type="button" className={styles.white_btn}>
-							Sign in
-						</button>
-					</Link>
-				</div>
-				<div className={styles.right}>
 					<form className={styles.form_container} onSubmit={handleSubmit}>
-						<h1>Create Account</h1>
-						<input
-							type="text"
-							placeholder="First Name"
-							name="firstName"
-							onChange={handleChange}
-							value={data.firstName}
-							required
-							className={styles.input}
-						/>
-						<input
-							type="text"
-							placeholder="Last Name"
-							name="lastName"
-							onChange={handleChange}
-							value={data.lastName}
-							required
-							className={styles.input}
-						/>
+						<h1>Login to Your Account</h1>
 						<input
 							type="email"
 							placeholder="Email"
@@ -92,11 +55,21 @@ export default function SignUp() {
 						/>
 						{error && <div className={styles.error_msg}>{error}</div>}
 						<button type="submit" className={styles.green_btn}>
-							Sign Up
+							Sign In
 						</button>
 					</form>
 				</div>
+				<div className={styles.right}>
+					<h1>New Here ?</h1>
+					<Link to="/signup">
+						<button type="button" className={styles.white_btn}>
+							Sign Up
+						</button>
+					</Link>
+				</div>
 			</div>
-		</div> 
-  )
-}
+		</div>
+	);
+};
+
+export default Login;
